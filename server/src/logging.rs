5b48e7a6cb7e37221c45eaac78415ff3889efdd2
xairaven <alexkovalevkpi@gmail.com>
2025-01-23 20:40:49 +0200

    (Server) Feat: Logging logic

---
+++ server/src/logging.rs
@@ -0,0 +1,24 @@
+use crate::errors::log::LogError;
+use chrono::Local;
+use log::LevelFilter;
+
+pub fn setup(log_level: LevelFilter) -> Result<(), LogError> {
+    if log_level == LevelFilter::Off {
+        return Ok(());
+    }
+
+    fern::Dispatch::new()
+        .level(log_level)
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{} {} {}] {}",
+                Local::now().format("%Y-%m-%d %H:%M"),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .chain(std::io::stdout())
+        .apply()
+        .map_err(LogError::SetLoggerError)
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let level_filter = log::LevelFilter::Info;
+
+    logging::setup(level_filter).unwrap_or_else(|err| {
+        let mut message = format!("Logger initialization failed. Error: {err}.");
+        if let Some(additional_info) = err.additional_info() {
+            message.push_str(&format!(" Additional_info: {additional_info}"));
+        }
+        println!("{}", message);
+        std::process::exit(1);
+    });
+}
+
+mod logging;
+mod errors {
+    pub mod log;
 }
