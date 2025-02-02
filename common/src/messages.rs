00d709553bdfc6a1aa7f2a8f64555292242e9192
xairaven <alexkovalevkpi@gmail.com>
2025-02-02 22:31:36 +0200

    (Common) Feat: `Reboot` command

--- common/src/messages.rs
+++ common/src/messages.rs
@@ -5,6 +5,7 @@ pub enum ClientRequest {
     RequestInterfaces,      // List of available ethernet interfaces
     SetInterface(String),   // Set an ethernet interface
     ChangePassword(String), // Change a password to another (not encrypted)
+    Reboot, // Reboot server (needed to apply changing password, for example)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -12,6 +13,7 @@ pub enum ServerResponse {
     InterfacesList(Vec<String>), // Available ethernet interfaces
     SetInterfaceResult(Result<(), ServerError>), // Is interface set by request?
     ChangePasswordResult(Result<(), ServerError>), // Is password changed by request?
+    RebootResult(Result<(), ServerError>), // Can reboot at the moment?
 
     Error(ServerError), // Generic Error.
 }
@@ -22,4 +24,5 @@ pub enum ServerError {
 
     InvalidInterface,
     FailedToChangePassword,
+    FailedToReboot,
 }
