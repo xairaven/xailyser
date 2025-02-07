4e7d9d511bf250cb9910a30fd6659ef2aaadd80b
xairaven <alexkovalevkpi@gmail.com>
2025-02-07 02:34:49 +0200

    (Server) Feat: Functions for getting `NetworkInterface` & `DataLinkReceiver`

--- server/src/net/interface.rs
+++ server/src/net/interface.rs
@@ -1,10 +1,13 @@
-use pnet::datalink::NetworkInterface;
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink::{DataLinkReceiver, NetworkInterface};
+use thiserror::Error;
 
 /// Usable interfaces. <br>
 /// Necessary: Presence of MAC address. <br>
 /// Necessary: Presence of IP (at least 1).
 pub fn usable_sorted() -> Vec<NetworkInterface> {
-    let mut interfaces: Vec<NetworkInterface> = pnet::datalink::interfaces()
+    let mut interfaces: Vec<NetworkInterface> = datalink::interfaces()
         .into_iter()
         .filter(|interface| interface.mac.is_some() && !interface.ips.is_empty())
         .collect();
@@ -13,4 +16,43 @@ pub fn usable_sorted() -> Vec<NetworkInterface> {
     interfaces.reverse();
 
     interfaces
+}
+
+/// Get `NetworkInterface` by its name.
+pub fn get_network_interface(
+    iface_name: &str,
+) -> Result<NetworkInterface, InterfaceError> {
+    let interface_names_match = |iface: &NetworkInterface| iface.name == iface_name;
+
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .find(interface_names_match)
+        .ok_or(InterfaceError::UnknownInterface)?;
+
+    Ok(interface)
+}
+
+pub fn get_datalink_channel(
+    interface: &NetworkInterface,
+) -> Result<Box<dyn DataLinkReceiver>, InterfaceError> {
+    let (_, rx) = match datalink::channel(interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => return Err(InterfaceError::UnhandledChannelType),
+        Err(err) => return Err(InterfaceError::UnableCreateChannel(err)),
+    };
+
+    Ok(rx)
+}
+
+#[derive(Debug, Error)]
+pub enum InterfaceError {
+    #[error("There are no interfaces with config interface name.")]
+    UnknownInterface,
+
+    #[error("There are no interfaces with config interface name.")]
+    UnhandledChannelType,
+
+    #[error("Unable to create channel.")]
+    UnableCreateChannel(std::io::Error),
 }
