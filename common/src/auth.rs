891824b2e0704f1d24bfdf9b3dcbf4f9b5771fc5
xairaven <alexkovalevkpi@gmail.com>
2025-04-17 17:51:18 +0300

    (Common) Feat: Compression auth header

--- common/src/auth.rs
+++ common/src/auth.rs
@@ -1,6 +1,9 @@
 pub const AUTH_HEADER: &str = "AUTH-KEY";
+pub const COMPRESSION_HEADER: &str = "COMPRESSION-VALUE";
 
 pub mod errors {
-    pub const HEADER_NOT_FOUND_ERROR: &str = "Password header was not found.";
-    pub const WRONG_PASSWORD_ERROR: &str = "Wrong password.";
+    pub const COMPRESSION_HEADER_NOT_FOUND: &str = "Compression header was not found.";
+    pub const PASSWORD_HEADER_NOT_FOUND: &str = "Password header was not found.";
+    pub const WRONG_COMPRESSION: &str = "Server has other compression settings.";
+    pub const WRONG_PASSWORD: &str = "Wrong password.";
 }
