80620b9ebc627f38e6098c087de5bed5b4f200aa
xairaven <alexkovalevkpi@gmail.com>
2025-04-28 01:05:39 +0300

    (DPI) Tests: Fixed ethernet test

--- dpi/src/protocols/ethernet.rs
+++ dpi/src/protocols/ethernet.rs
@@ -30,6 +30,19 @@ pub fn parse<'a>(bytes: &'a [u8], _: &FrameMetadata) -> IResult<&'a [u8], Protoc
     Ok((rest, ProtocolData::Ethernet(layer)))
 }
 
+pub fn best_children(metadata: &FrameMetadata) -> Option<ProtocolId> {
+    // Checking ethernet ether type
+    let ethernet = match metadata.layers.first() {
+        Some(ProtocolData::Ethernet(value)) => value,
+        _ => return None,
+    };
+    match ethernet.ether_type {
+        EtherType::Arp => Some(ProtocolId::Arp),
+        EtherType::Ipv4 => Some(ProtocolId::IPv4),
+        _ => None,
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct Ethernet {
     pub id: ProtocolId,
@@ -56,37 +69,25 @@ pub enum EthernetError {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::ProtocolParser;
-    use crate::frame::FrameType;
     use crate::wrapper::FrameHeader;
 
     #[test]
     fn test_ethernet() {
         let hex_actual = "40 61 86 9A F1 F5 00 1A 8C 15 F9 80 08 00 45 00 00 28 59 B0 40 00 38 06 86 FC CE 6C CF 8B C0 A8 03 83 00 50 DA 98 A4 28 53 A0 9A 18 FA A4 50 10 00 D8 E5 69 00 00 00 00 00 00 00 00".replace(" ", "");
         let frame = hex::decode(hex_actual).unwrap();
-        let header = FrameHeader {
-            tv_sec: 0,
-            tv_usec: 0,
-            caplen: 60,
-            len: 0,
-        };
-
-        let parser = ProtocolParser::new(&pcap::Linktype(1), false);
-        let packet = pcap::Packet {
-            header: &pcap::PacketHeader::from(&header),
-            data: &frame,
-        };
-        let result = parser.process(packet);
-        let metadata = match result {
-            Some(value) => match value {
-                FrameType::Metadata(value) => value,
-                FrameType::Raw(_) => panic!(),
+        let metadata = FrameMetadata {
+            header: FrameHeader {
+                tv_sec: 0,
+                tv_usec: 0,
+                caplen: 60,
+                len: 0,
             },
-            None => panic!(),
+            layers: vec![],
         };
 
-        let actual_ethernet = match metadata.layers[0].clone() {
-            ProtocolData::Ethernet(value) => value,
+        let layer = parse(&frame, &metadata);
+        let actual_ethernet = match layer {
+            Ok((_, ProtocolData::Ethernet(layer))) => layer,
             _ => panic!(),
         };
 
