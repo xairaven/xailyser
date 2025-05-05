d35ca0b1d89dc1c54430c70831a2f7fc2d676988
xairaven <alexkovalevkpi@gmail.com>
2025-05-05 03:03:04 +0300

    (Common) Feat: Broadcast pool, new version of broadcast channel

--- common/src/channel.rs
+++ common/src/channel.rs
@@ -1,4 +1,43 @@
 use crossbeam::channel::{Receiver, Sender, TrySendError};
+use std::collections::VecDeque;
+
+pub struct BroadcastPool<T: Clone> {
+    senders: VecDeque<Sender<T>>,
+    receivers: VecDeque<Receiver<T>>,
+}
+
+impl<T: Clone> Default for BroadcastPool<T> {
+    fn default() -> Self {
+        Self {
+            senders: VecDeque::with_capacity(8),
+            receivers: VecDeque::with_capacity(8),
+        }
+    }
+}
+
+impl<T: Clone> BroadcastPool<T> {
+    pub fn create(&mut self) {
+        let (tx, rx) = crossbeam::channel::unbounded::<T>();
+        self.senders.push_back(tx);
+        self.receivers.push_back(rx);
+    }
+
+    pub fn last_receiver(&mut self) -> Option<Receiver<T>> {
+        self.receivers.pop_front()
+    }
+
+    pub fn is_receiver_ready(&self) -> bool {
+        !self.receivers.is_empty()
+    }
+
+    pub fn last_sender(&mut self) -> Option<Sender<T>> {
+        self.senders.pop_back()
+    }
+
+    pub fn is_sender_ready(&self) -> bool {
+        !self.senders.is_empty()
+    }
+}
 
 pub struct BroadcastChannel<T: Clone> {
     senders: Vec<Sender<T>>,
@@ -17,10 +56,8 @@ impl<T: Clone> BroadcastChannel<T> {
         }
     }
 
-    pub fn add_receiver(&mut self) -> Receiver<T> {
-        let (tx, rx) = crossbeam::channel::unbounded::<T>();
-        self.senders.push(tx);
-        rx
+    pub fn add_sender(&mut self, sender: Sender<T>) {
+        self.senders.push(sender);
     }
 
     pub fn send(&mut self, msg: T) {
@@ -31,9 +68,11 @@ impl<T: Clone> BroadcastChannel<T> {
                 disconnected.push(index);
             }
         }
-        disconnected.reverse();
-        for index in disconnected {
-            self.senders.remove(index);
+        if !disconnected.is_empty() {
+            disconnected.reverse();
+            for index in disconnected {
+                self.senders.remove(index);
+            }
         }
     }
 
