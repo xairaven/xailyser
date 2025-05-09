9f52f49dc376f186b3ada4d49f8743279e649d85
xairaven <alexkovalevkpi@gmail.com>
2025-05-09 04:56:05 +0300

    (Client) Feat: Generic function for protocol views, DHCPv4 view

--- client/src/ui/tabs/inspector.rs
+++ client/src/ui/tabs/inspector.rs
@@ -23,18 +23,18 @@ impl InspectorTab {
         self.select_view(ui);
 
         match self.protocol_chosen {
-            ProtocolId::Ethernet => {},
             ProtocolId::Arp => self.arp_view(ui, ctx),
-            ProtocolId::IPv4 => {},
-            ProtocolId::IPv6 => {},
-            ProtocolId::ICMPv4 => {},
-            ProtocolId::ICMPv6 => {},
-            ProtocolId::TCP => {},
-            ProtocolId::UDP => {},
-            ProtocolId::DHCPv4 => {},
+            ProtocolId::DHCPv4 => self.dhcpv4_view(ui, ctx),
             ProtocolId::DHCPv6 => {},
             ProtocolId::DNS => {},
+            ProtocolId::Ethernet => {},
             ProtocolId::HTTP => {},
+            ProtocolId::ICMPv4 => {},
+            ProtocolId::ICMPv6 => {},
+            ProtocolId::IPv4 => {},
+            ProtocolId::IPv6 => {},
+            ProtocolId::TCP => {},
+            ProtocolId::UDP => {},
         };
     }
 
@@ -60,8 +60,13 @@ impl InspectorTab {
         });
     }
 
-    pub fn arp_view(&mut self, ui: &mut egui::Ui, ctx: &mut Context) {
-        let storage = &mut ctx.net_storage.inspector.arp;
+    fn protocol_view<T, F>(
+        ui: &mut egui::Ui, storage: &mut Vec<T>, grid_id: &str, num_columns: usize,
+        headings: &[&str], mut render_row: F,
+    ) where
+        F: FnMut(&mut egui::Ui, usize, &T),
+    {
+        // Clear button or empty label
         if !storage.is_empty() {
             ui.vertical_centered_justified(|ui| {
                 if ui.button(t!("Button.Clear")).clicked() {
@@ -72,46 +77,83 @@ impl InspectorTab {
             ui.label(RichText::new(t!("Tab.Inspector.Label.Empty")).italics());
         }
 
-        ScrollArea::vertical()
+        // Table
+        ScrollArea::both()
             .auto_shrink([false, true])
             .show(ui, |ui| {
-                Grid::new("Inspector.Arp.Packets")
+                Grid::new(grid_id)
                     .striped(true)
-                    .num_columns(6)
+                    .num_columns(num_columns)
                     .show(ui, |ui| {
+                        // Headings row
                         if !storage.is_empty() {
-                            ui.label(styles::heading::grid(&t!(
-                                "Tab.Inspector.Label.Number"
-                            )));
-                            ui.label(styles::heading::grid(&t!(
-                                "Tab.Inspector.Protocol.Arp.Operation"
-                            )));
-                            ui.label(styles::heading::grid(&t!(
-                                "Tab.Inspector.Protocol.Arp.IpSender"
-                            )));
-                            ui.label(styles::heading::grid(&t!(
-                                "Tab.Inspector.Protocol.Arp.IpTarget"
-                            )));
-                            ui.label(styles::heading::grid(&t!(
-                                "Tab.Inspector.Protocol.Arp.MacSender"
-                            )));
-                            ui.label(styles::heading::grid(&t!(
-                                "Tab.Inspector.Protocol.Arp.MacTarget"
-                            )));
-                        }
-                        ui.end_row();
-
-                        for (index, packet) in storage.iter().enumerate() {
-                            ui.label(format!("{}", index));
-                            ui.label(format!("{}", packet.operation));
-                            ui.label(format!("{}", packet.sender_ip));
-                            ui.label(format!("{}", packet.sender_mac));
-                            ui.label(format!("{}", packet.target_ip));
-                            ui.label(format!("{}", packet.target_mac));
+                            for &h in headings {
+                                ui.label(styles::heading::grid(&t!(h)));
+                            }
                             ui.end_row();
                         }
-                    })
+
+                        // Data rows
+                        for (idx, packet) in storage.iter().enumerate() {
+                            render_row(ui, idx, packet);
+                            ui.end_row();
+                        }
+                    });
             });
+    }
+
+    pub fn arp_view(&mut self, ui: &mut egui::Ui, ctx: &mut Context) {
+        let storage = &mut ctx.net_storage.inspector.arp;
+        InspectorTab::protocol_view(
+            ui,
+            storage,
+            "Inspector.Arp.Packets",
+            6,
+            &[
+                "Tab.Inspector.Label.Number",
+                "Tab.Inspector.Protocol.Arp.Operation",
+                "Tab.Inspector.Protocol.Arp.IpSender",
+                "Tab.Inspector.Protocol.Arp.IpTarget",
+                "Tab.Inspector.Protocol.Arp.MacSender",
+                "Tab.Inspector.Protocol.Arp.MacTarget",
+            ],
+            |ui, idx, packet| {
+                ui.label(idx.to_string());
+                ui.label(packet.operation.to_string());
+                ui.label(packet.sender_ip.to_string());
+                ui.label(packet.target_ip.to_string());
+                ui.label(packet.sender_mac.to_string());
+                ui.label(packet.target_mac.to_string());
+            },
+        );
+    }
+
+    pub fn dhcpv4_view(&mut self, ui: &mut egui::Ui, ctx: &mut Context) {
+        let storage = &mut ctx.net_storage.inspector.dhcpv4;
+        InspectorTab::protocol_view(
+            ui,
+            storage,
+            "Inspector.DHCPv4.Packets",
+            7,
+            &[
+                "Tab.Inspector.Label.Number",
+                "Tab.Inspector.Protocol.DHCPv4.MessageType",
+                "Tab.Inspector.Protocol.DHCPv4.OldClientAddress",
+                "Tab.Inspector.Protocol.DHCPv4.NewClientAddress",
+                "Tab.Inspector.Protocol.DHCPv4.ServerAddress",
+                "Tab.Inspector.Protocol.DHCPv4.RelayAgentAddress",
+                "Tab.Inspector.Protocol.DHCPv4.ClientMAC",
+            ],
+            |ui, idx, packet| {
+                ui.label(idx.to_string());
+                ui.label(packet.message_type.to_string());
+                ui.label(packet.old_client_address.to_string());
+                ui.label(packet.new_client_address.to_string());
+                ui.label(packet.server_address.to_string());
+                ui.label(packet.relay_agent_address.to_string());
+                ui.label(packet.hardware_address_client.to_string());
+            },
+        );
     }
 
     fn tab_heading(&self, ui: &mut egui::Ui, ctx: &mut Context) {
