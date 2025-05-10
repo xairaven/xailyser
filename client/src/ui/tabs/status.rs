8828c8e53532eca596b0732cfca0da18b3cbc29b
xairaven <alexkovalevkpi@gmail.com>
2025-05-11 01:41:43 +0300

    (Client) Fix: Horizontal wrapped unsaved pcap

--- client/src/ui/tabs/status.rs
+++ client/src/ui/tabs/status.rs
@@ -4,7 +4,7 @@ use crate::ui::components::throughput_settings::ThroughputSettings;
 use crate::ui::modals::message::MessageModal;
 use crate::ui::styles;
 use crate::ui::tabs::Tab;
-use egui::{RichText, ScrollArea};
+use egui::{Grid, RichText, ScrollArea};
 
 pub struct StatusTab {
     throughput_settings: ThroughputSettings,
@@ -142,36 +142,47 @@ impl StatusTab {
 
     fn pcap_save_view(&mut self, ui: &mut egui::Ui, ctx: &mut Context) {
         if !ctx.net_storage.raw.is_empty() {
-            ui.horizontal(|ui| {
-                ui.label(format!("Unparsed Frames: {}", ctx.net_storage.raw.amount()));
-                if ui.button("Save .pcap").clicked() {
-                    let link_type = match ctx.settings_server.link_type {
-                        Some(value) => value,
-                        None => {
-                            MessageModal::error(&t!("Error.FailedUnpackLinkType"))
-                                .try_send_by(&ctx.modals_tx);
-                            return;
-                        },
-                    };
+            Grid::new("UnparsedFramesControls")
+                .num_columns(3)
+                .striped(false)
+                .show(ui, |ui| {
+                    ui.label(format!(
+                        "Unparsed Frames: {}",
+                        ctx.net_storage.raw.amount()
+                    ));
+                    if ui.button("Save .pcap").clicked() {
+                        let link_type = match ctx.settings_server.link_type {
+                            Some(value) => value,
+                            None => {
+                                MessageModal::error(&t!("Error.FailedUnpackLinkType"))
+                                    .try_send_by(&ctx.modals_tx);
+                                return;
+                            },
+                        };
 
-                    if let Some(path) = rfd::FileDialog::new()
-                        .add_filter(net::PCAP_FILTER_NAME, net::PCAP_FILTER_EXTENSIONS)
-                        .save_file()
-                    {
-                        if let Err(err) = ctx.net_storage.raw.save_pcap(path, link_type) {
-                            MessageModal::error(&format!(
-                                "{}: {}",
-                                &t!("Error.Pcap"),
-                                err
-                            ))
-                            .try_send_by(&ctx.modals_tx);
+                        if let Some(path) = rfd::FileDialog::new()
+                            .add_filter(
+                                net::PCAP_FILTER_NAME,
+                                net::PCAP_FILTER_EXTENSIONS,
+                            )
+                            .save_file()
+                        {
+                            if let Err(err) =
+                                ctx.net_storage.raw.save_pcap(path, link_type)
+                            {
+                                MessageModal::error(&format!(
+                                    "{}: {}",
+                                    &t!("Error.Pcap"),
+                                    err
+                                ))
+                                .try_send_by(&ctx.modals_tx);
+                            }
                         }
                     }
-                }
-                if ui.button("Reset").clicked() {
-                    ctx.net_storage.raw.clear();
-                }
-            });
+                    if ui.button("Reset").clicked() {
+                        ctx.net_storage.raw.clear();
+                    }
+                });
         }
     }
 }
